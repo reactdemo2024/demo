0572b1a55917c9f9563e35a65f2b234f5ccd5179
Kiara Melocoton <kiaramelocoton@gmail.com>
2024-06-26 16:40:50 -0700

    refactor: fix deployment homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 // import reportWebVitals from "./reportWebVitals"
 // import * as serviceWorker from "./serviceWorker"
